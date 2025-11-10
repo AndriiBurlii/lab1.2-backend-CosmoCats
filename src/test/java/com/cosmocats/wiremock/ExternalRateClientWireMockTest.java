9123d69019c2ffe5280f1ccb6213f15a95c274e2
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-11-10 12:48:37 +0200

    tests: add MockMvc integration tests, WireMock @SpringBootTest, and Mockito unit tests

--- src/test/java/com/cosmocats/wiremock/ExternalRateClientWireMockTest.java
+++ src/test/java/com/cosmocats/wiremock/ExternalRateClientWireMockTest.java
@@ -1,31 +1,30 @@
-
 package com.cosmocats.wiremock;
 
 import com.cosmocats.client.ExternalRateClient;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 @WireMockTest(httpPort = 8089)
 class ExternalRateClientWireMockTest {
 
+    @Autowired
+    ExternalRateClient client;
+
     @Test
     void wiremock_stubbing_ok() {
-        // Arrange
-        configureFor("localhost", 8089);
         stubFor(get(urlEqualTo("/status"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "text/plain")
                         .withBody("OK")));
-        ExternalRateClient client = new ExternalRateClient();
 
-        // Act
         String status = client.getStatus("http://localhost:8089");
-
-        // Assert
         assertEquals("OK", status);
         verify(getRequestedFor(urlEqualTo("/status")));
     }
