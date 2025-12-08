b994ae1f7b03398d1ec3510ebff71851473a66bc
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-08 14:17:04 +0200

    Update FeatureToggleAspect.java

--- src/main/java/com/cosmocats/service/FeatureToggleAspect.java
+++ src/main/java/com/cosmocats/service/FeatureToggleAspect.java
@@ -3,15 +3,16 @@ package com.cosmocats.service;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class FeatureToggleAspect {
 
     private static final Logger log = LoggerFactory.getLogger(FeatureToggleAspect.class);
+
     private final FeatureToggleService toggles;
 
     public FeatureToggleAspect(FeatureToggleService toggles) {
@@ -19,13 +20,17 @@ public class FeatureToggleAspect {
     }
 
     @Around("@annotation(flag)")
-public Object aroundFeatureMethod(ProceedingJoinPoint pjp, FeatureFlag flag) throws Throwable {
-    String name = flag.value();
-    boolean on = toggles.isEnabled(name);
-    log.info("Feature '{}' enabled={}", name, on);
-    if (on) {
-        return pjp.proceed();
+    public Object aroundFeatureMethod(ProceedingJoinPoint pjp, FeatureFlag flag) throws Throwable {
+        String name = flag.value();
+        boolean on = toggles.isEnabled(name);
+
+        log.info("Feature '{}' enabled={}", name, on);
+
+        if (on) {
+            return pjp.proceed();
+        }
+
+        // exception сам формує текст повідомлення
+        throw new FeatureNotAvailableException(name);
     }
-    // тепер exception сам формує текст
-    throw new FeatureNotAvailableException(name);
 }
