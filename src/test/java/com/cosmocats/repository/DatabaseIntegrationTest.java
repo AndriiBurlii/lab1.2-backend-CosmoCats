5aea9b9156689ebe2b224cac922b7f8d6cf35a09
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-09 23:13:13 +0200

    Update DatabaseIntegrationTest.java

--- src/test/java/com/cosmocats/repository/DatabaseIntegrationTest.java
+++ src/test/java/com/cosmocats/repository/DatabaseIntegrationTest.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
+import com.cosmocats.entity.OrderLineEntity;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
