63ce45289d1a46c7f7e80293223ee61838c58ffc
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-10 01:08:11 +0200

    Create ProductAlreadyExistsException.java

---
+++ src/main/java/com/cosmocats/service/ProductAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.cosmocats.service;
+
+public class ProductAlreadyExistsException extends RuntimeException {
+
+    public ProductAlreadyExistsException(String name) {
+        super("Product with name '" + name + "' already exists");
+    }
+}
