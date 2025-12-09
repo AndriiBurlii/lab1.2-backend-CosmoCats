21cdb349a84478fb81ab82bd3a279c81a9fdb6e8
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-10 00:56:45 +0200

    Create ProductNotFoundException.java

---
+++ src/main/java/com/cosmocats/service/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.cosmocats.service;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(long id) {
+        super("Product with id " + id + " not found");
+    }
+}
