95a3b5f5f19e0f45e4625bbcd33e4406c611c5c1
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-09 23:01:50 +0200

    Update OrderLineRepository.java

--- src/main/java/com/cosmocats/repository/OrderLineRepository.java
+++ src/main/java/com/cosmocats/repository/OrderLineRepository.java
@@ -1,7 +1,7 @@
 package com.cosmocats.repository;
 
-import com.cosmocats.domain.OrderLine;
+import com.cosmocats.entity.OrderLineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
+public interface OrderLineRepository extends JpaRepository<OrderLineEntity, Long> {
 }
