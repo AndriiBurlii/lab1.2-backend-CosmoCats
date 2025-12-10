403c2bcecebcaf7461ce3063d847f5eed114a95d
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-10 14:59:26 +0200

    Create JwtAuthConverter.java

---
+++ src/main/java/com/cosmocats/config/JwtAuthConverter.java
@@ -0,0 +1,36 @@
+package org.example.cosmocats.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JwtAuthConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        // приклад для Keycloak: realm_access.roles
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        if (realmAccess == null) {
+            return List.of();
+        }
+
+        Object rolesObject = realmAccess.get("roles");
+        if (!(rolesObject instanceof List<?> roles)) {
+            return List.of();
+        }
+
+        return roles.stream()
+                .filter(String.class::isInstance)
+                .map(String.class::cast)
+                .map(role -> "ROLE_" + role) // важливо: ROLE_ prefix
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+}
