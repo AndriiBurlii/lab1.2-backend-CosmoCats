dfc529a5c31dcdd9f10069de91fe9d15597a4f8f
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-10 17:22:20 +0200

    Update ProductControllerValidationTest.java

--- src/test/java/com/cosmocats/api/ProductControllerValidationTest.java
+++ src/test/java/com/cosmocats/api/ProductControllerValidationTest.java
@@ -6,11 +6,12 @@ import com.cosmocats.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class ProductControllerValidationTest {
 
     @Autowired
