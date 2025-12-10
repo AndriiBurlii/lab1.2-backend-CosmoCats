c524dcffcd56de90bc05628d39cc4a63fb066a3c
AndriiBurlii <burliy.andriy@lll.kpi.ua>
2025-12-10 16:19:34 +0200

    Update SecurityConfig.java

--- src/main/java/com/cosmocats/config/SecurityConfig.java
+++ src/main/java/com/cosmocats/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.example.cosmocats.config;
+package com.cosmocats.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
